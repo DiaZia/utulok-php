7108ddb2efa5b74946c426da6cf71b1c628bc182
Diana Žiaková <dianaziakova@gmail.com>
2023-12-02 18:57:19 +0100

    fw vaiicko + root page

---
+++ App/Views/Home/index.view.php
@@ -0,0 +1,59 @@
+<div id="navigation"></div>
+<main class="mainDiv">
+    <div class="row">
+        <div class="content col-sm-10 col-md-10">
+            <div id="carouselExampleCaptions" class="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="public/images/pets1.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Adoptuj si najlepšieho kamaráta</h5>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="public/images/pets2.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Zachráň život nevinnému zvieratku</h5>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="public/images/pets3.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Podpor našu dobrovoľnícku činnosť</h5>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+            <div class="introduction">
+                <h2>Vitaj na našej stránke</h2>
+                <p>
+                    Náš útulok pre zvieratá je miestom, kde sa o zvieratá staráme, ochraňujeme ich a hľadáme im nové domovy. Naša práca má hlboké korene a za sebou bohatú históriu.
+                    Naša vízia je jasná: zabezpečiť, aby žiadne zviera nebolo opustené, utláčané alebo bez lásky. Urobili sme veľa pre to, aby sme túto víziu premenili na realitu. Sme tu, aby sme zmenili príbehy týchto zvierat, aby im priniesli nádej a zázraky, ktoré si zaslúžia.
+                    Zároveň chceme byť tiež hlasom pre zvieratá vo svete a osvečovať verejnosť o dôležitosti etického zaobchádzania s nimi. Naša prítomnosť v komunite je zárukou toho, že ani jedno zviera nebude opustené alebo zneužité bez následkov.
+                    Pozrite sa na naše služby, dozviete sa viac o našich príbehoch a načrtnite cestu, ako sa môžete zapojiť do tejto nádhernej misie. Spoločne môžeme zachrániť životy a šíriť lásku.
+                    Vaša podpora je nám neoceniteľná, a sme vďační za každého, kto sa rozhodne pridať k našej komunite. Vpred, na ceste k spoločnej láske a starostlivosti o naše chlpatých a šupinatých priateľov.
+                </p>
+            </div>
+        </div>
+        <aside class="col-sm-2 col-md-2">
+            <div class="asidePanel">
+                <img src="public/images/facebook.jpg" alt="facebook">
+                <img src="public/images/instagram.jpg" alt="instagram">
+            </div>
+        </aside>
+    </div>
+</main>
+<div id="footer"></div>
---
+++ App/Views/root.layout.view.php
@@ -0,0 +1,48 @@
+<?php
+
+/** @var string $contentHTML */
+/** @var \App\Core\IAuthenticator $auth */
+/** @var \App\Core\LinkGenerator $link */
+?>
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <title><?= \App\Config\Configuration::APP_NAME ?></title>
+    <title>Útulok</title>
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <script src="public/js/script.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg">
+    <div class="row header">
+        <div class="col-6 col-sm-6 col-md-6 col-lg-4"><a class="title">Útulok Pacička</a></div>
+        <div class="col-4 col-sm-4 col-md-4 col-lg-7">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a href="o_nas.html" class="mainButtons">O nás</a>
+                    <li class="nav-item"><a href="nase_zvieratka.html" class="mainButtons">Naše zvieratká</a></li>
+                    <li class="nav-item"><a href="#" class="mainButtons">Podporte nás</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-2 col-sm-2 col-md-2 col-lg-1">
+            <img src="public/images/login.jpg" alt="login" class="loginPic" onclick="window.location.href='login.html'">
+        </div>
+    </div>
+</nav>
+<div class="container-fluid mt-3">
+    <div class="web-content">
+        <?= $contentHTML ?>
+    </div>
+</div>
+</body>
+</html>
